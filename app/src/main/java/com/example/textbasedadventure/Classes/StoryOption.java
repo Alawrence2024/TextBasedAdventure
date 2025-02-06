d87a941a5e4c3ad97b1680b6660df0e26091d101
ZekeCali <jmcnelis940@gmail.com>
2025-02-06 09:47:25 -0700

    Added StoryOption.java to hold dialog and what options come next

---
+++ app/src/main/java/com/example/textbasedadventure/Classes/StoryOption.java
@@ -0,0 +1,10 @@
+package com.example.textbasedadventure.Classes;
+
+import java.util.ArrayList;
+
+public class StoryOption {
+
+    public ArrayList<StoryOption> nextOptions;
+    public String optionTitle;
+    public String optionText;
+}
