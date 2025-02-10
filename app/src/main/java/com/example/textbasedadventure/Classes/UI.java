6c918dae5b7eae0a979ed70c86aa7643ccb04dc8
lbailey52 <160799070+lbailey52@users.noreply.github.com>
2025-02-10 09:56:20 -0700

    added UI class for click events

---
+++ app/src/main/java/com/example/textbasedadventure/Classes/UI.java
@@ -0,0 +1,23 @@
+package com.example.textbasedadventure.Classes;
+
+import android.view.View;
+
+public class UI {
+
+    public void onClickOpt1(MouseEvent event)
+    {
+
+    }    
+    public void onClickOpt2(MouseEvent event)
+    {
+
+    }
+    public void onClickOpt3(MouseEvent event)
+    {
+
+    }
+    public void onClickOpt4(MouseEvent event)
+    {
+
+    }
+}
