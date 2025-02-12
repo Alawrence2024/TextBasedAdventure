6761986e0b961bea33c49ced8e34dd115224c08f
lbailey52 <160799070+lbailey52@users.noreply.github.com>
2025-02-11 19:29:15 -0700

    UI background adjustment

--- app/src/main/java/com/example/textbasedadventure/MainActivity2.java
+++ app/src/main/java/com/example/textbasedadventure/MainActivity2.java
@@ -2,12 +2,14 @@ package com.example.textbasedadventure;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
 
     Button btnOpt1, btnOpt2, btnOpt3, btnOpt4;
+    TextView tvInfo1, tvInfo2, tvInfo3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,12 @@ public class MainActivity2 extends AppCompatActivity {
         btnOpt2 = findViewById(R.id.btnOpt2);
         btnOpt3 = findViewById(R.id.btnOpt3);
         btnOpt4 = findViewById(R.id.btnOpt4);
+
+        tvInfo1 = findViewById(R.id.tvInfo1);
+        tvInfo2 = findViewById(R.id.tvInfo2);
+        tvInfo3 = findViewById(R.id.tvInfo3);
+
+        tvInfo1.setText("Hello Welcome");
 
         btnOpt1.setOnClickListener(v -> {
             // Handle button click
