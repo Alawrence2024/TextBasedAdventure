dab961bb820793ebe7566ce21781c18882c5b69d
FiniteGuide9147 <158355100+FiniteGuide9147@users.noreply.github.com>
2025-02-13 10:59:57 -0700

    Created Class folder Created ClassBase as a parent class -Added the six main stats as variables (strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma) and added getters and setters --Setters have if statements to insure the inputted int is not above the maxStatScore or below the minStatScore; throws Exception if it is -Added className variable and added getters and setters -Added classLevel variable and added getters and setters --Setter has if statements to insure the inputted int is not above the maxClassLevel or below the minClassLevel; throws Exception if it is Created classes for the main four class options (Rogue, Fighter, Druid, and Sorcerer) -Made them extend the parent class, ClassBase 02/10 (Hannah): Changes to ClassBase: -added modifiers to the 6 base stats (str, dex, int, wis, con, cha) -added getters and setters for the modifiers (setters take the number from the base stat and set the modifier based on that) -added armorClass (ac) -added getter and setter for ac -added proficencyBonus -added getter and setter for proficiencyBonus (setter takes the class level and sets the profbonus based on that) Changes to Rogue: -added variable to hold sneak attack damage -added getter and setter for sneak attack damage variable (setter takes classLevel and sets the sneak dmg based on that) 02/11 (Hannah): Changes to ClassBase: -added hitPoints and getter and setter --setter takes in hitDiceSides, converts it to an int and multiplies it with hitDiceNumber, then sets result to hitPoints -added hitDiceSides and getter with abstract setter -added hitDiceNumber with getter and setter --setter sets hitDiceNumber to be equal to classLevel Changes to Rogue: -implemented abstract hitDiceSides setter --sets hitDiceSides to "d8" -added uncannyDodge method --takes in an int (attackDamage) and returns it (after halving it if classLevel is equal to or above 5) -made constructor for Rogue class that calls all set methods to fill in necessary stats 02/11 (Hannah): -Made new folder (DndClasses) and moved my dnd classes classes into it 02/13 (Hannah): Changes to ClassBase: -added new updateHitPoints method --takes in current hit points and the change and updates the hitpoints -added maxHitPoints and getter --keeps track of what the character's max hit point total is. Is set in the hitPoints setter after the hitPoints are set Changes to Rogue: -honestly, i am not sure. I guess i deleted an unneeded line or something Changes to Fighter: -implemented setHitDiceSides method --sets hitDicesides to d10 -added secondWind method --generates a random number between 0-10, sets 0 to 1 if generated, adds fighter level and adds the total to the hitPoints. -made constructor for Fighter class. 02/13 (Hannah): Changes to Druid: -uncommented the class out -Added constructor -Implimented hitDiceSides method --sets hitDiceSides to "d8" -added method bonusToWisdomAndConstitutionChecks --takes in the check result and a boolean asking if it was a wisdom check --if it was a wisdom check, it will add the proficiency bonus and the normal bonus to the result and return it --otherwise it just adds the normal bonus to the result and returns it Changes to Sorcerer: -uncommented the class out -Added constructor -Implimented hitDiceSides method --sets hitDiceSides to "d8" -added method bonusToResultOnCharismaOrIntelligenceBasedChecks --takes in the check result and a boolean asking if it was a charisma check --if it was a charisma check, it will add the proficiency bonus and the normal bonus to the result and return it --otherwise it just adds the normal bonus to the result and returns it

--- app/src/main/java/com/example/textbasedadventure/Classes/DndClasses/Druid.java
+++ app/src/main/java/com/example/textbasedadventure/Classes/DndClasses/Druid.java
@@ -1,4 +1,43 @@
 package com.example.textbasedadventure.Classes.DndClasses;
 
-//public class Druid extends ClassBase {
-//}
+public class Druid extends ClassBase {
+
+    public Druid(int strength, int dexterity, int constitution, int wisdom, int intelligence, int charisma, int classLevel, int armorClass) throws Exception {
+        setClassName("Druid");
+        setClassLevel(classLevel);
+        setStrength(strength);
+        setStrengthModifier();
+        setDexterity(dexterity);
+        setDexterityModifier();
+        setConstitution(constitution);
+        setConstitutionModifier();
+        setWisdom(wisdom);
+        setWisdomModifier();
+        setIntelligence(intelligence);
+        setIntelligenceModifier();
+        setCharisma(charisma);
+        setCharismaModifier();
+        setProficiencyBonus();
+        setArmorClass(armorClass);
+        setHitdiceSides();
+        setHitDiceNumber();
+        setHitPoints();
+    }
+
+    @Override
+    void setHitdiceSides() {
+        hitDiceSides = "d8";
+    }
+
+    public int bonusToResultOnWisdomOrConstitutionBasedChecks (int result, boolean isWisdomCheck){
+        int newResult = result;
+        int bonus = 2;
+        if (isWisdomCheck){
+            int profBonus = Integer.parseInt(getProficiencyBonus());
+            newResult = result + profBonus + bonus;
+        } else {
+            newResult = result + bonus;
+        }
+        return newResult;
+    }
+}
--- app/src/main/java/com/example/textbasedadventure/Classes/DndClasses/Sorcerer.java
+++ app/src/main/java/com/example/textbasedadventure/Classes/DndClasses/Sorcerer.java
@@ -1,4 +1,43 @@
 package com.example.textbasedadventure.Classes.DndClasses;
 
-//public class Sorcerer extends ClassBase {
-//}
+public class Sorcerer extends ClassBase {
+
+    public Sorcerer(int strength, int dexterity, int constitution, int wisdom, int intelligence, int charisma, int classLevel, int armorClass) throws Exception {
+        setClassName("Sorcerer");
+        setClassLevel(classLevel);
+        setStrength(strength);
+        setStrengthModifier();
+        setDexterity(dexterity);
+        setDexterityModifier();
+        setConstitution(constitution);
+        setConstitutionModifier();
+        setWisdom(wisdom);
+        setWisdomModifier();
+        setIntelligence(intelligence);
+        setIntelligenceModifier();
+        setCharisma(charisma);
+        setCharismaModifier();
+        setProficiencyBonus();
+        setArmorClass(armorClass);
+        setHitdiceSides();
+        setHitDiceNumber();
+        setHitPoints();
+    }
+
+    @Override
+    void setHitdiceSides() {
+        hitDiceSides = "d6";
+    }
+
+    public int bonusToResultOnCharismaOrIntelligenceBasedChecks (int result, boolean isCharismaCheck){
+        int newResult = result;
+        int bonus = 2;
+        if (isCharismaCheck){
+            int profBonus = Integer.parseInt(getProficiencyBonus());
+            newResult = result + profBonus + bonus;
+        } else {
+            newResult = result + bonus;
+        }
+        return newResult;
+    }
+}
