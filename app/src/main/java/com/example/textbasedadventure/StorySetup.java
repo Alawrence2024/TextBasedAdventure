d4de718e6dc9684e8f9d41207b31c0c2c56b56a6
Alawrence2024 <159195154+Alawrence2024@users.noreply.github.com>
2025-02-25 04:25:28 -0700

    Added story options

--- app/src/main/java/com/example/textbasedadventure/StorySetup.java
+++ app/src/main/java/com/example/textbasedadventure/StorySetup.java
@@ -12,15 +12,46 @@ public class StorySetup {
 
     public StorySetup() {
         startNode.optionTitle = "Journey's Start";
-        startNode.optionText = "You have arrived in the town of Kerfta, where strange happenings have occurred. Choose your next move wisely.";
+        startNode.optionText = "Arriving in town you see signs of a once lively sprawl, deserted market stalls like the streets, children’s toys lay discarded in the street. As you walk through the town’s modest square you feel the eyes of suspicious onlookers as daggers upon your backs. One brave soul works up the courage to confront you and your compatriots “What be your business? Be quick with it and go. This town has had too much trouble for adventurers like yourselves to attract more.";
 
-        StoryOption townSquareOption = new StoryOption("Speak with the townsfolk", "The locals seem uneasy, but perhaps one of them can provide useful information.");
-        StoryOption blacksmithOption = new StoryOption("Visit the Blacksmith", "You might be able to gather more supplies for your journey.");
-        StoryOption dukeCastleOption = new StoryOption("Go to the Duke's Castle", "The Duke might have more information about the beast haunting the town.");
 
-        startNode.nextOptions.add(townSquareOption);
-        startNode.nextOptions.add(blacksmithOption);
-        startNode.nextOptions.add(dukeCastleOption);
+
+
+
+        //Blacksmith tree
+        StoryOption townBlacksmith = new StoryOption("We are just looking for a blacksmith", "The blacksmith lives at the edge of town. You lot will be a sight for sore eyes. Hasn’t had much business since things began");
+        StoryOption townSmithTransition = new StoryOption("Continue", "You can always hear a smithy before you see it. The familiar clang of steel being molded to the will of the smith’s hammer echoes through your ears. You approach a tall figure perfecting their craft. \n\n" + "“Looking for some protection adventurer?” A bulky woman covered in many a scar inquires.");
+        StoryOption blacksmith = new StoryOption("“Our blades are proficient, but we could all use some new armor. Ours are on their last legs”", "“Had a run in with the beast then? Tell me was it hairy or scaly, I’ve got a wager to win. Oh what am I saying you wouldn’t be alive if you’d seen it. Sorry, I can’t do much for you. A cave in at the mine of Mt. Yor’Thune has left me without workable ore.”");
+        StoryOption blacksmithYorthuneTransition = new StoryOption("Go to Mt. Yor'Thune", "Mt. Yor’thune, a towering spire of earth, juts out from the worlds crust. It’s white peaks, loom over you if only to remind you that you’re place in this life is insignificant. A caved in mineshaft is framed by the base of the mountain.");
+
+        startNode.nextOptions.add(townBlacksmith);
+        startNode.nextOptions.get(0).nextOptions.add(townSmithTransition);
+        startNode.nextOptions.get(0).nextOptions.get(0).nextOptions.add(blacksmith);
+        startNode.nextOptions.get(0).nextOptions.get(0).nextOptions.get(0).nextOptions.add(blacksmithYorthuneTransition);
+
+        //Horses Tail inn tree
+        StoryOption townInn = new StoryOption("We need a place to stay the night", "“The Horses Tail might still be open. But don’t think of staying too long, adventurers attract all sorts of mischief.”");
+        StoryOption townInnTransition = new StoryOption("Continue", "A pitiful trail of smoke trails from the chimney of this cozy downtown inn. Though it’s windows are boarded up you see a small sign swinging in the light breeze “VACANCY“. You enter hoping to find some shelter for the night.\n\n" + "“Oh. Dear, we’ve guests!” A kind elderly looking woman beacons to her portly husband crouched behind the bar. “Will you be staying with us this evening?”");
+        StoryOption inn = new StoryOption("We were hoping to rest here the night, but it seems rather unsafe?", "Yes, admittedly things haven’t be as peaceful as they usually are. You see people have been going missing lately. Most say something about a hairy monster that lives in the nearby forest and comes at dusk. Though I’m not sure anyone has lived to properly see the creature. But I assure you this humble inn of ours is the safest place to be tonight.");
+        StoryOption innStay = new StoryOption("Stay the night","While you are fast asleep the beast stealthy climbs into your rooms window and carries you off never to be seen again.");
+
+        startNode.nextOptions.add(townInn);
+        startNode.nextOptions.get(1).nextOptions.add(townInnTransition);
+        startNode.nextOptions.get(1).nextOptions.get(0).nextOptions.add(inn);
+        startNode.nextOptions.get(1).nextOptions.get(0).nextOptions.get(0).nextOptions.add(innStay);
+
+        //General Store tree
+        StoryOption townGeneralStore = new StoryOption("We are looking for supplies", "“General store still has some supplies, I think. Best hurry before their bought up”");
+        StoryOption townStoreTransition = new StoryOption("Continue", "A well-kept general store sits before you, and general it is. You could swear you’ve seen this exact store a dozen times or more on your travels. You enter, hoping that they have at least some trail rations left in stock.\n\n" + "“What ever it is, we’re OUT” A scraggly bearded man proclaims as the door opens. “Ah adventurers, ignore what I just said. I’m sure I can find something you’d be interested in.”");
+        StoryOption generalStore = new StoryOption("“We were in the market for some trail rations, though it looks you couldn’t find even a mere crumb here. Where have all your wares gone?”", "“I have to admit, it’s true. Haven’t been able to get the supply wagons through. They’re too scared the creature that stalks the Hills of Gelyle will snatch their horses right from under them. Truly it’s not that far, once you past the forest you could make your way through the hills and get supplies in the town next over.”");
+        StoryOption storeForestTransition = new StoryOption("Head to the forest", "The Gelyian Woods sits in the center of the county, providing travelers with plentiful shade and refuge from the rain. As you approach you see several fallen trees and deep scratch marks on their bark. One particularly large tree lay in your path. ");
+
+        startNode.nextOptions.add(townGeneralStore);
+        startNode.nextOptions.get(2).nextOptions.add(townStoreTransition);
+        startNode.nextOptions.get(2).nextOptions.get(0).nextOptions.add(generalStore);
+        startNode.nextOptions.get(2).nextOptions.get(0).nextOptions.get(0).nextOptions.add(storeForestTransition);
+
+
 
         finalBattleNode.optionTitle = "Final Battle";
         finalBattleNode.optionText = "You have reached the beast's lair. It's time to face the monster that has haunted this town.";
@@ -55,10 +86,9 @@ public class StorySetup {
 
     // Druid-specific options
     private void addDruidOptions() {
-        StoryOption townInfoOption = new StoryOption("Ask about the forest", "You inquire about the strange happenings in the forest, sensing a disturbance in nature.");
+        StoryOption townInfoOption = new StoryOption("Blacksmith", "You inquire about the strange happenings in the forest, sensing a disturbance in nature.");
         StoryOption townInfoBeast = new StoryOption("Ask about the beast", "You ask the townsfolk about the beast, hoping to understand its origins.");
-        startNode.nextOptions.get(0).nextOptions.add(townInfoOption);
-        startNode.nextOptions.get(0).nextOptions.add(townInfoBeast);
+
 
         // Blacksmith Options for Druid
         StoryOption blacksmithInteraction = new StoryOption("Speak with the Blacksmith", "The Blacksmith seems busy but notices your druidic attire and asks if you can assist with repairing his enchanted tools.");
@@ -69,6 +99,12 @@ public class StorySetup {
         startNode.nextOptions.get(2).nextOptions.add(dukeIntro);
         StoryOption leaveDuke = new StoryOption("Leave", "The Duke was concerned by you leaving suddenly");
         startNode.nextOptions.get(2).nextOptions.add(leaveDuke);
+        StoryOption test = new StoryOption("test", "test");
+        startNode.nextOptions.get(2).nextOptions.add(test);
+        StoryOption test2 = new StoryOption("test2", "test2");
+        startNode.nextOptions.get(2).nextOptions.get(2).nextOptions.add(test2);
+        StoryOption test2alt = new StoryOption("test2alt", "test2alt");
+        startNode.nextOptions.get(2).nextOptions.get(2).nextOptions.add(test2alt);
     }
 
     // Fighter-specific options
@@ -91,20 +127,14 @@ public class StorySetup {
 
     // Rogue-specific options
     private void addRogueOptions() {
-        StoryOption townInfoOptionRogue = new StoryOption("Ask about the beast", "You ask the townsfolk if they have any information on the beast's treasure hoard.");
-        StoryOption townInfoSecrets = new StoryOption("Ask about secrets", "You listen carefully to the rumors, hoping to learn something that others might have missed.");
-        startNode.nextOptions.get(0).nextOptions.add(townInfoOptionRogue);
-        startNode.nextOptions.get(0).nextOptions.add(townInfoSecrets);
-
-        // Blacksmith Options for Rogue
-        StoryOption blacksmithInteractionRogue = new StoryOption("Speak with the Blacksmith", "The Blacksmith eyes you warily but offers you a set of lockpicks and light armor, perfect for sneaking past the beast.");
-        startNode.nextOptions.get(1).nextOptions.add(blacksmithInteractionRogue);
+        StoryOption townInfoOptionRogue = new StoryOption("Trouble?", "Aye a ghastly beast has accosted the town for nearly a fortnight. If we keep to ourselves and stay inside the casualties aren’t as bad\\n\\n" + "f you insist on bringing this town more misery speak to the duke. Now off with yeh!");
+        StoryOption townCastleTransition = new StoryOption("Continue", "By all accords this must be the smallest castle you’ve seen on your journeys. Though even the most destitute noble is still a noble. A pair of guards escort you into an extensively decorated main hall. Before you sits the Duke of Gelyle, a thin man for his position with a young physique. He sits with his head in his hand, a torn bright red cloth clutched in his right hand. Noticing he sobs: \n\n" + "What business do people of fortune have in my county? I have no time for you, can’t you see I am in mourning?");
+        StoryOption wineDine = new StoryOption("Wine and dine the duke","My daughter, the apple of my eye, I can’t believe she’s gone. That horrid thing, dragging her off to its lair. Not to mention some of the townsfolk spreading the rumor that I did such a horrible thing. \\n\\n" +  "Something seems off about the way he speaks, you can hear it in his voice. He is trying too hard to make a point that it couldn’t be him.");
 
         // Duke's Castle Options for Rogue
-        StoryOption dukeIntroRogue = new StoryOption("Speak with the Duke", "The Duke seems curious about your skills as a rogue, offering a special reward for anyone who can sneak past the beast's defenses.");
-        startNode.nextOptions.get(2).nextOptions.add(dukeIntroRogue);
-        StoryOption leaveDuke = new StoryOption("Leave", "The Duke was concerned by you leaving suddenly");
-        startNode.nextOptions.get(2).nextOptions.add(leaveDuke);
+        startNode.nextOptions.add(townInfoOptionRogue);
+        startNode.nextOptions.get(3).nextOptions.add(townCastleTransition);
+        startNode.nextOptions.get(3).nextOptions.get(0).nextOptions.add(wineDine);
     }
 
     // Sorcerer-specific options
